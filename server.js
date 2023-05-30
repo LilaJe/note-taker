24b474e43d194602205a26d24af6935e2fa38773
Lila Ellis <lilaellis@lilas-mpb.lan>
2023-05-29 20:06:22 -0500

    added routes for static files

--- server.js
+++ server.js
@@ -5,8 +5,18 @@ const app = express();
 // || stands for OR, so if process.env.PORT is not available, use 3000
 const PORT = process.env.PORT || 3000;
 
+// define middleware for serving static files
+// a static file is that does not need to be generated.
+app.use(express.static("public"));
+
+app.get("/notes", (req, res) => {
+  console.log(JSON.stringify(req.query));
+  res.sendFile(__dirname + "/public/notes.html");
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  // dirname is the directory of the current file.
+  res.sendFile(__dirname + "/public/index.html");
 });
 
 app.listen(PORT, () => {
