cce3878111e90d6af84f34ea76d66f1f1cf972a9
Lila Ellis <lilaellis@lilas-mpb.lan>
2023-05-31 00:36:05 -0500

    writing read/write logic

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,67 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
 const router = express.Router();
+
+const dataBaseFile = path.join(__dirname, "../db/db.json");
+console.log(dataBaseFile);
 
 router.get("/notes", (req, res) => {
   console.log(JSON.stringify(req.query));
-  res.send("Hello from the API route!");
+  fs.readFile(dataBaseFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end();
+    }
+
+    res.json(JSON.parse(data));
+  });
 });
 
 router.post("/notes", (req, res) => {
   console.log(JSON.stringify(req.query));
-  res.send("Hello from the API route!");
+  fs.readFile(dataBaseFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end();
+    }
+
+    const notes = JSON.parse(data);
+    const newNote = { ...req.body, id: uuidv4() };
+    notes.push(newNote);
+
+    fs.writeFile(dataBaseFile, JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).end();
+      }
+
+      res.json(newNote);
+    });
+  });
 });
 
-router.delete("/notes/:id", (req, res) => {
-  res.send("Hello from the API route!");
+router.delete("/api/notes/:id", (req, res) => {
+  fs.readFile(dataBaseFile, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).end();
+    }
+
+    let notes = JSON.parse(data);
+    notes = notes.filter((note) => note.id !== req.params.id);
+
+    fs.writeFile(dataBaseFile, JSON.stringify(notes, null, 2), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).end();
+      }
+
+      res.json({ message: "Note deleted" });
+    });
+  });
 });
 
 module.exports = router;
